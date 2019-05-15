40a33f9c032318094f22f866bc1ddcbe410531aa
alabi sodiq <adisco4420@gmail.com>
2019-05-15 17:06:44 +0100

    confirm and resend email route

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const JoiValidator = require('../middlewares/validator');
 const UserController = require('../controller/UserController');
-const {RegisterUserValidator} = require('../validations/UserValidator')
+const {RegisterUserValidator, ConfirmUserValidator, ResendEmailValidator} = require('../validations/UserValidator')
 const router = express.Router();
 
-router.post('/register', JoiValidator(RegisterUserValidator), UserController.RegisterUser)
+router.post('/register', JoiValidator(RegisterUserValidator), UserController.RegisterUser);
+router.post('/confirm', JoiValidator(ConfirmUserValidator), UserController.ConfirmUser);
+router.post('/resend', JoiValidator(ResendEmailValidator), UserController.ResendEmail);
 
 module.exports = router;
