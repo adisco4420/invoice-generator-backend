4230880a09a4fcb86443564b28aea42a325d45d3
Sodiq Alabi <adisco4420@gmail.com>
2019-05-16 18:47:42 +0100

    Revert "Add contacts"

--- validations/ContactValidator.js
+++
@@ -1,7 +0,0 @@
-const joi = require('joi');
-exports.AddContactValidator = {
-  fullName: joi.string().required(),
-  email: joi.string().email({ minDomainSegments: 2 }),
-  phoneNumber: joi.number(),
-  address: joi.string()
-};
