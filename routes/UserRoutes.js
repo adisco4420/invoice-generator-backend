d22b47bd9a3d47c00de9e842b885133b31a0f139
alabi sodiq <adisco4420@gmail.com>
2019-05-16 11:16:46 +0100

    setup route

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const JoiValidator = require('../middlewares/validator');
 const UserController = require('../controller/UserController');
-const {RegisterUserValidator, ConfirmUserValidator, ResendEmailValidator} = require('../validations/UserValidator')
+const {RegisterUserValidator, ConfirmUserValidator, ResendEmailValidator, SetupUserValidator} = require('../validations/UserValidator')
+const AuthMiddleWare = require('../middlewares/auth');
 const router = express.Router();
 
 router.post('/register', JoiValidator(RegisterUserValidator), UserController.RegisterUser);
 router.post('/confirm', JoiValidator(ConfirmUserValidator), UserController.ConfirmUser);
 router.post('/resend', JoiValidator(ResendEmailValidator), UserController.ResendEmail);
-
+router.patch('/setup', JoiValidator(SetupUserValidator), AuthMiddleWare,  UserController.SetupUser)
 module.exports = router;
